2522c297f4bacc6e4dac2ee59484d592ee1dacae
Oleg <oleg-kozlov91@rambler.ru>
2024-04-20 16:55:45 +0300

    123

--- index.f67df751.js
+++ index.f67df751.js
@@ -2,13 +2,16 @@ const btnOpen = document.querySelector("#openmodal");
 const overlayBg = document.querySelector(".overlay__wrapper");
 const modalOverlay = document.querySelector(".overlay");
 const btnClose = document.querySelector(".close");
-const openSearch = document.querySelector("#search__btn");
+const openSearch = document.querySelectorAll(".search__btn");
 const submitSearch = document.querySelector(".modal__submit");
 const modalSearch = document.querySelector(".modal__wrap");
 const modalCheck = document.querySelector(".modal__checkbox");
-openSearch.addEventListener("click", ()=>{
-    modalSearch.classList.remove("modal__wrap");
-    modalSearch.classList.add("modal__wrap_active");
+openSearch.forEach((e)=>{
+    e.addEventListener("click", (e)=>{
+        e.preventDefault();
+        modalSearch.classList.remove("modal__wrap");
+        modalSearch.classList.add("modal__wrap_active");
+    });
 });
 submitSearch.addEventListener("click", ()=>{
     modalSearch.classList.remove("modal__wrap_active");
